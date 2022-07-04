570bbbdc67594c82157166f935aaa910201e3d5c
Ahmad <fire_emblem_1@hotmail.com>
2022-07-04 20:40:24 +0300

    task done

--- calculator.py
+++ calculator.py
@@ -1,10 +1,37 @@
+def get_calculation(num1, num2, operation):
+    if operation == '+':
+        return num1 + num2
+    elif operation =='-':
+        return num1 - num2
+    elif operation == '*':
+        return num1*num2
+    elif operation == '/':
+        return num1/num2 
+
+    else: return None
+
+
+
 
 def main():
-	#write your code here
-	pass
-	
+    num1=input("enter first number:")
+    num2= input("enter second number:")
+    if num1.isdigit()and num2.isdigit():
+
+        operation = input ("choose your opertation(*,/,+,-")
+        answer= get_calculation(int(num1),int(num2),operation)
+
+        if answer == None:
+            print("the operation is not valid")
+        else:
+            print("the answer is "+str(answer))
+    else:
+        print ("the input is not a number")
+main()
+pass 
 
 
 
-if __name__ == '__main__':
-	main()
+
+if name == 'main':
+    main()
